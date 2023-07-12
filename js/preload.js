f2d6fd2896568e9791fe2124ad4abe5c8b61e6fb
Ray <rainday17539@gmail.com>
2023-07-12 10:14:33 +0800

    修改preload animation

--- js/preload.js
+++ js/preload.js
@@ -1,83 +1,54 @@
-    // 在整個頁面內容載入完成後執行的程式碼
+// 在整個頁面內容載入完成後執行的程式碼
 
-    let circle = document.querySelector(".circle");
-    let textArray = [];
-    textArray = circle.innerText.split("");
+let circle = document.querySelector(".circle");
+let textArray = [];
+textArray = circle.innerText.split("");
+let runAniIndex = 0;
 
-    let runAniIndex = 0;
+function runAnimation(callback) {
 
-    // //禁止滑鼠和手機滑動動作
-    // disableScroll(function () {
-    //   // 開放滑鼠和手機滑動動作
-    //   enableScroll();
-    // });
-
-    // ------------------------------------------------
-
-    // function disableScroll(callback) {
-    //   // 禁止滑鼠滾動事件
-    //   document.addEventListener("wheel", preventDefaultScroll, { passive: false });
-    //   // 禁止手機觸摸滑動事件
-    //   document.addEventListener("touchmove", preventDefaultScroll, { passive: false });
-    //   setTimeout(function () {
-    //     callback();
-    //   }, 4000)
-    // }
-
-    // function enableScroll() {
-    //   // 啟用滑鼠滾動事件
-    //   document.removeEventListener("wheel", preventDefaultScroll);
-    //   // 啟用手機觸摸滑動事件
-    //   document.removeEventListener("touchmove", preventDefaultScroll);
-    // }
-
-    // function preventDefaultScroll(event) {
-    //   event.preventDefault();
-    // }
-
-    function runAnimation(callback) {
-
-      if (runAniIndex < textArray.length) {
-        let spans = document.querySelectorAll("span");
-        // console.log(spans);
-        for (let i = 0; i < spans.length; i++) {
-          spans[i].classList.remove("jump");
-        }
-
-        //貼新的單字
-        let strHTML = `<span data-num="${runAniIndex}" class="jump">${textArray[runAniIndex]}</span>`;
-        circle.innerHTML += strHTML;
-
-        //抓取作後一個單字
-        let spans2 = document.querySelectorAll("span");
-        // console.log(spans2);
-        let lastspan = spans2[spans2.length - 1];
-
-        //監聽作後一個單字動畫結束
-        lastspan.addEventListener("animationend", function () {
-          runAniIndex++;
-          runAnimation(callback)
-        })
-      } else {
-        runAniIndex = 0;
-        callback();
-      }
-
+  if (runAniIndex < textArray.length) {
+    //清空之前的 animation
+    let spans = circle.querySelectorAll("span");
+    for (let i = 0; i < spans.length; i++) {
+      spans[i].classList.remove("jump");
     }
 
-    // ------------------------------------------------
+    //貼新的單字
+    let strHTML = `<span data-num="${runAniIndex}" class="jump">${textArray[runAniIndex]}</span>`;
+    circle.innerHTML += strHTML;
 
-    //執行開場動畫
-    $("body").css("overflow","hidden")
-    $(".circle").empty();
-    runAnimation(function () {
-      $(".preload").addClass("fade");
-      setTimeout(function () {
-        $(".preload").css("display", "none");
-        $("body").css("overflow","auto")
-      }, 1000)
+    //抓取作後一個單字
+    let spans2 = circle.querySelectorAll("span");
+    // console.log(spans2);
+    let lastspan = spans2[spans2.length - 1];
 
-    });
+      //監聽作後一個單字動畫結束
+      lastspan.addEventListener("animationend", function () {
+        runAniIndex++;
+        runAnimation(callback)
+      })
+    } else {
+      runAniIndex = 0;
+      callback();
+    }
 
-    // ------------------------------------------------
+}
+
+
+//執行開場動畫
+$(".all-wrap").css("display","none");
+$(".circle").empty();
+runAnimation(function () {
+  $(".preload").addClass("fade");
+  setTimeout(function () {
+    $(".all-wrap").css("display","block");
+  }, 500)
+  setTimeout(function () {
+    $(".all-wrap").addClass("fadeReverse");
+    $(".preload").css("display", "none");
+  }, 1000)
+
+});
+
 
